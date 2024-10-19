13f40ad9f76b4cd623e471a1ee4ff05defdf64e4
ModitTdr <modittuladhar@gmail.com>
2024-10-19 17:39:28 +0545

    update

--- public/script.js
+++ public/script.js
@@ -1,2 +1,18 @@
-var test = document.getElementsByTagName('span');
-test.style.color="red";
+var tog = document.querySelector(".tog");
+var darkMode = localStorage.getItem("darkMode") === "true";
+tog.checked = darkMode;
+if(darkMode) darkEnable();
+
+tog.addEventListener("change", (e) => {
+  if (tog.checked) {
+    darkEnable();
+  } else {
+    darkDisable();
+  }
+});
+function darkEnable() {
+  localStorage.setItem("darkMode", true);
+}
+function darkDisable() {
+  localStorage.setItem("darkMode", false);
+}
